00f145c9e4c8abf74365e5d752f549982aab7209
dekujon <joniantumabang@gmail.com>
2023-05-15 16:11:28 +0800

    completed pokedex v1.3

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -4,9 +4,9 @@ function PokemonList({ pokemon, pokeOnClick }) {
       <ul className="pokelist">
         {pokemon.map((p) => (
           <li key={p.name} onClick={() => pokeOnClick(p.id)} className="poke-card">
-            <p className="pokeName capitalize">{p.name}</p>
+            <p className="pokeName capitalize hide">{p.name}</p>
             <img src={p.image} alt={p.name} />
-            <p className="pokeName">#{p.id}</p>
+            <p className="pokeName hide">#{p.id}</p>
           </li>
         ))}
       </ul>
